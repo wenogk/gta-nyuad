9b41e2f2ac2fc179d7015569faeb06954642111d
youssef-ahamid <ya1027@nyu.edu>
2020-06-24 03:52:56 +0400

    fixed typos on descriptions

--- scripts/missionContent.js
+++ scripts/missionContent.js
@@ -4,24 +4,25 @@ addMissionContent(`
   `,0,162, false);
 addMissionContent(`
   Select your clothes <br />
-  Here you just started the game and you went into GTA mod. You have been wearing the same clothes for so long, it's time for you to change your outfit. So choose your outfit to start.
+  This is the start of the game. Your outfit is not fit for a criminal, so it's time for you to change it. Go over your options and pick your outfit to begin your crime life.
   `,163,233,false);
 addMissionContent(`
     Get Money&Sanatizer <br />
-    This is your first mission. In the flash back do you remember how Romeno refused to give you money, it's revenge time. Go get money from him and Sanatizer, and beat Romeno up along the way so he learns his lesson.
+    Remember how Romeno refused to give you money and hand sanitizer? It's time for your revenge. Get $$$ and sanatizer, and maybe beat him up along the way so he learns his lesson.
     `,244,285, true);
 addMissionContent(`
   Acquire a Bike <br />
-  This is your second mission. When you wanted to borrow a bike so bad and you were feeling down, Saad was very rude to you and told you to get lost. It's to time for Saad to lose his bike.
+   When you wanted to borrow a bike and you were feeling down, Saad was very rude to you and told you to get lost. 
+  It's time for Saad to lose his bike.
 
   `,286,342, true);
   addMissionContent(`
     Steal Monister Energy Drink <br />
-  This is your third mission. You have been broke for the past week, spending your money on alchohol and ciggerates. Convenience Store refused to give you monister energy drink so its time to steal it.
+   You have been broke for the past week, spending all your money on alchohol and cigarettes. The convenience Store refused to give you a monster energy drink so its time for you to steal it.
 
     `,343,401, true);
     addMissionContent(`
       Failed Mission <br />
-      You caused enough problems in NYU, an dno student did what you did today. The dean is very pissed at you and he decided to suspend you. Life is too short to car, so enjoy a walk on campus while it lasts.
+      You got caught stealing. The dean is very pissed at you and he decided to suspend you. Life is too short to care, so enjoy a walk on campus while it lasts.
 
       `,401,492,false);
